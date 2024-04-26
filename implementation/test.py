9d13c96fb4a7d2279a106a4dab85f43620b0b0d8
sakuni jayasinghe <s.nimnadi@gmail.com>
2024-04-26 21:59:33 +0530

    q learning initial recommendation

--- implementation/test.py
+++ implementation/test.py
@@ -30,22 +30,7 @@ user_preferences = {'Happy': {'music': ['Drama', ' Romance'], 'movies': ['Drama'
 book_df = pd.read_csv("./datasets/book_filtered_data.csv", sep = ",")
 movie_df = pd.read_csv("./datasets/movie_filtered_data.csv", sep = ",")
 music_df = pd.read_csv("./datasets/music_filtered_data.csv", sep = ",")
-# print(book_df)
-# Function to give initial recommendations based on user preferences
-# def initial_recommendation(user_preferences, df1, df2, df3):
-#     recommendations = {}
-#     for state, preferences in user_preferences.items():
-#         music_recommendations = music_df[music_df['genre'].isin(preferences['music'])]['name'].head(2).tolist()
-#         book_recommendations = book_df[book_df['genre'].isin(preferences['books'])]['name'].head(2).tolist()
-#         movie_recommendations = movie_df[movie_df['genre'].isin(preferences['movies'])]['name'].head(2).tolist()
-        
-#         recommendations[state] = {'music': music_recommendations, 'books': book_recommendations, 'movies': movie_recommendations}
-    
-#     return recommendations
 
-# # Example usage
-# initial_recommendations = initial_recommendation(user_preferences, music_df, book_df, movie_df)
-# print("Initial recommendations:", initial_recommendations)
 
 # # Function to give initial recommendations based on user preferences
 # def initial_recommendation(user_preferences, df1, df2, df3):
